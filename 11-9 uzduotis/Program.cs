e2fdc02d561d12950bcc2e8a96c1fce0f8bea1c9
Ieva <ieva.jaksaityte@gmail.com>
2018-01-23 20:26:58 +0200

    uzduotys

---
+++ 11-9 uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_9_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suma = 0;
+
+            for (int i = 20; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // prideti i skaiciu prie sumos
+                }
+            }
+
+            // isvesti gauta atsakyma
+        }
+    }
+}

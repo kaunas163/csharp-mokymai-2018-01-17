b67443d1b318e30c087539c1e47c75710afe5573
Ieva <ieva.jaksaityte@gmail.com>
2018-01-23 19:41:12 +0200

    uzduotis

---
+++ 11-7 uzduotis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_7_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite du skaicius");
+            var pirmas = Convert.ToInt32(Console.ReadLine());
+            var antras = Convert.ToInt32(Console.ReadLine());
+
+            var atsakymas = 0;
+
+            for (int i = 0; i < antras; i++)
+            {
+                //atsakymas += pirmas;
+                atsakymas = atsakymas + pirmas;
+            }
+
+            Console.WriteLine("daugyba: " + atsakymas);
+        }
+    }
+}

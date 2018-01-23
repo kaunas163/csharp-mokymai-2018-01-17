0cc8ea731c22e67b8fa1df4d9855eb1a49c08228
Ieva <ieva.jaksaityte@gmail.com>
2018-01-23 21:29:48 +0200

    pavyzdys

---
+++ 12-6 random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_6_random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomas = new Random();
+            var suma = 0;
+            var didziausias = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                // pirmas budas:
+                //Console.Write(randomas.Next(20) + " "); // [0-20)
+
+                // antras budas:
+                var skaicius = randomas.Next(5, 50); // [5-50)
+                Console.Write(skaicius + " ");
+
+                suma += skaicius;
+
+                if (skaicius > didziausias)
+                {
+                    didziausias = skaicius;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("suma: " + suma);
+            Console.WriteLine("didziausias: " + didziausias);
+        }
+    }
+}

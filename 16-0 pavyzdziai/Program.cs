b9c28bd235be0e62a051dd4288630fd2680f47cf
Ieva <ieva.jaksaityte@gmail.com>
2018-01-26 20:44:19 +0200

    pavyzdziai

---
+++ 16-0 pavyzdziai/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_0_pavyzdziai
+{
+    class Matematika
+    {
+        public static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Skirtumas(int a, int b)
+        {
+            var skirtumas = a - b;
+            return skirtumas;
+        }
+
+        public static int Sandauga(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Dalmuo(int a, int b)
+        {
+            return (double) a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Pasisveikinti();
+            Atsisveikinti();
+            
+            var programa = new Program(); // objektas, kuriamas viena karta
+            programa.Pinigai();
+            programa.Automobilis();
+
+            var suma = Matematika.Suma(5, 4);
+            Console.WriteLine(suma);
+
+            Console.WriteLine(Matematika.Suma(4, 8));
+
+            // iskviesti visus likusius metodus
+
+            Console.WriteLine(Matematika.Skirtumas(7, 5));
+            Console.WriteLine(Matematika.Sandauga(5, 4));
+            Console.WriteLine(Matematika.Dalmuo(7, 5));
+        }
+
+        public static void Pasisveikinti()
+        {
+            Console.WriteLine("jusu vardas?");
+            Console.WriteLine("Labas, " + Console.ReadLine());
+        }
+
+        public static void Atsisveikinti()
+        {
+            Console.WriteLine("viso gero");
+        }
+
+        public void Pinigai()
+        {
+            Console.WriteLine("Pasakykite ka daro pinigai");
+            var pinigai = Console.ReadLine();
+            Console.WriteLine("aciu, jusu atsakymas priimtas, jis yra:");
+            Console.WriteLine(pinigai);
+        }
+
+        public void Automobilis()
+        {
+            Console.WriteLine("ka vairuojate?");
+            var auto = Console.ReadLine();
+            if (auto == "kamaz")
+            {
+                Console.WriteLine("nice");
+            }
+        }
+    }
+}
---
+++ 16-0-1 pavyzdziai/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_0_1_pavyzdziai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nuvaziuotiKilometrazai = new List<int>();
+            Program programa = new Program();
+
+            programa.Ivedimas(nuvaziuotiKilometrazai);
+            programa.Isvedimas(nuvaziuotiKilometrazai, "PRADINIAI DUOMENYS");
+            programa.Skaiciavimai(nuvaziuotiKilometrazai);
+
+            List<int> atrinkti = programa.Atrinkimas(nuvaziuotiKilometrazai);
+            programa.Isvedimas(atrinkti, "ATRINKTI DUOMENYS");
+
+        } // metodo main pabaiga
+
+        public void Ivedimas(List<int> sarasas)
+        {
+            var dar = true;
+
+            while (dar)
+            {
+                Console.Write("iveskite kilometraza: ");
+                var km = Convert.ToInt32(Console.ReadLine());
+                sarasas.Add(km);
+
+                Console.WriteLine("norit kartoti? t/n");
+                var kartoti = Console.ReadLine();
+                if (kartoti != "t")
+                {
+                    dar = false;
+                }
+            }
+        }
+
+        public void Isvedimas(List<int> sarasas, string komentaras)
+        {
+            Console.WriteLine(komentaras);
+
+            foreach (var km in sarasas)
+            {
+                Console.Write("[{0}]", km);
+            }
+
+            Console.WriteLine();
+        }
+
+        public int Suma(List<int> sarasas)
+        {
+            return sarasas.Sum();
+        }
+
+        public int Maziausias(List<int> sarasas)
+        {
+            return sarasas.Min();
+        }
+
+        public int Didziausias(List<int> sarasas)
+        {
+            return sarasas.Max();
+        }
+
+        public double Vidurkis(List<int> sarasas)
+        {
+            var vidurkis = (double) Suma(sarasas) / sarasas.Count;
+            return vidurkis;
+        }
+
+        public void Skaiciavimai(List<int> sarasas)
+        {
+            Console.WriteLine("Suma: " + Suma(sarasas));
+            Console.WriteLine("Min: " + Maziausias(sarasas));
+            Console.WriteLine("Max: " + Didziausias(sarasas));
+            Console.WriteLine("Vidurkis: " + Vidurkis(sarasas));
+        }
+
+        public List<int> Atrinkimas(List<int> pradiniai)
+        {
+            // daugiau nei 300k, laikysim jau kaip graba
+
+            var atrinkti = new List<int>();
+
+            foreach (var km in pradiniai)
+            {
+                if (km > 300000)
+                {
+                    atrinkti.Add(km);
+                }
+            }
+
+            return atrinkti;
+        }
+
+    } // class program pabaiga
+}
